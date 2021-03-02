3e6da1b62e45283cb345a6dff8d5398106792a21
Justin Klein <JustinKln51@gmail.com>
2021-03-02 15:14:12 +0100

    added router and json file

--- routes/pokemon/index.js
+++ routes/pokemon/index.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
+
+router.get('/:pokeid', (req, res) => {
+    var pokeid = req.params.pokeid;
+    var poke = pokemon.findOne({'pokeid': pokeid});
+
+    if (poke) {
+        res.send(poke);
+    } else {
+        res.status(404).send(`Pokemon with Pokedex-ID ${pokeid} not found!`);
+    }
+});
